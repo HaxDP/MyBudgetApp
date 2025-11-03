a44cf2514cc4f6814e6f9cc43fb58accee145f81
HaxDP <brod49155@gmail.com>
2025-11-03 21:52:53 +0200

    Version 1.1.0.2 edited server.js for hosting

--- MyBudgetBack/server.js
+++ MyBudgetBack/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = require('./db');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
