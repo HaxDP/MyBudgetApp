317cd09ca6cb3d720c0b1666a2befab580a5bbbf
HaxDP <brod49155@gmail.com>
2025-11-03 22:54:38 +0200

    Version 1.1.0.3 edited server.js for hosting

--- MyBudgetBack/server.js
+++ MyBudgetBack/server.js
@@ -7,7 +7,19 @@ const pool = require('./db');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+const allowedOrigins = [
+    'https://monumental-alpaca-f9eea3.netlify.app'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 app.use(express.json());
 
 app.post('/register', async (req, res) => {
