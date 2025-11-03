57260f2c06430213ed4527b4d189230c36d6cb94
HaxDP <brod49155@gmail.com>
2025-11-03 23:23:47 +0200

    Version 1.1.0.7 edited package.json and db.js for hosting

--- MyBudgetFront/app.js
+++ MyBudgetFront/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://mybudgetapp-production-8c3a.up.railway.app';
 
 function getUserData() {
     const userDataString = localStorage.getItem('myBudgetAppUser');
